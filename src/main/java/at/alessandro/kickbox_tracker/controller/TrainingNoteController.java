3eee3e8ab45a1b6a7a8c8ce0d7872acd00b9233a
Alessandro Crispino <alessandro_crispino@gmx.at>
2026-06-30 18:49:31 +0200

    Add CORS config for frontend communication

--- src/main/java/at/alessandro/kickbox_tracker/controller/TrainingNoteController.java
+++ src/main/java/at/alessandro/kickbox_tracker/controller/TrainingNoteController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/notes")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TrainingNoteController {
     private final TrainingNoteService service;
 
--- src/main/java/at/alessandro/kickbox_tracker/controller/TrainingSessionController.java
+++ src/main/java/at/alessandro/kickbox_tracker/controller/TrainingSessionController.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sessions")
+@CrossOrigin(origins = "http://localhost:5173")
 public class TrainingSessionController{
     private final TrainingSessionService service;
 
